094009231d05a3b3cd80d32439197c4ca4812aed
AdamAbd <adamabd14113@gmail.com>
2022-03-09 21:38:31 +0700

    add content to each page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi,
+        cupiditate alias explicabo fugiat est non ut doloremque dolores aliquid,
+        aut animi dolorum? Odit esse necessitatibus fugit eius voluptates
+        tempore illum dolorum nam? Laudantium, ab voluptatum nam id asperiores
+        recusandae voluptate vel officia officiis repellat maiores totam quas
+        aspernatur! Consequuntur quibusdam vel aliquid amet dicta saepe! Esse
+        error sint maxime? Cupiditate, veniam assumenda at sed deleniti natus
+        nesciunt doloribus nostrum libero ut nemo minus quo repellendus
+        doloremque tempora! Delectus, in impedit.
+      </p>
+    </div>
+  );
 }
 
 export default Contact;
