1f00e3c075978d27ab7d7f64300ecbedd953b5a0
AdamAbd <adamabd14113@gmail.com>
2022-03-09 22:53:03 +0700

    connecting blog page with api

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Blog() {
-  return <div>Blog</div>;
+  const [blogs, setBlog] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function () {
+    async function getBlog() {
+      const response = await fetch(
+        "https://api.spaceflightnewsapi.net/v3/blogs"
+      );
+      const data = await response.json();
+
+      setBlog(data);
+      setLoading(false);
+      // console.log(data);
+    }
+    getBlog();
+  }, []);
+
+  return (
+    <section>
+      <h1>Blog</h1>
+      <p>Tulisan-tulisan ku tapi boong</p>
+      {loading ? (
+        <i>Loading Blog ...</i>
+      ) : (
+        <div>
+          {blogs.map(function (blog) {
+            return <article key={blog.id}>{blog.title}</article>;
+          })}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default Blog;
