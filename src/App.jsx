fffd112327ae1323efddbd97a3383b2040ac03f8
AdamAbd <adamabd14113@gmail.com>
2022-03-09 21:27:30 +0700

    add file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <nav>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"profile"}>Profile</Link>
+        </li>
+        <li>
+          <Link to={"blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link to={"contact"}>Contact</Link>
+        </li>
+      </nav>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"profile"} element={<Profile />} />
+        <Route path={"blog"} element={<Blog />} />
+        <Route path={"contact"} element={<Contact />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
